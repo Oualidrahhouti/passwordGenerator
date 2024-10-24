24533239a533226963bc825abf78aaac0a3fc881
oualidrahhouti <rahhoutiwalid@gmail.com>
2024-10-24 19:22:07 +0200

    adding properties and improving template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,40 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  title = 'Angular Password Generator';
+
+  length = 6;
+
+  uppercase = false;
+
+  numbers = false;
+
+  symbols = false;
+
+  password = '';
+
+  onChangeLength(event: Event) {
+    const parsedValue = parseInt((event.target as HTMLInputElement).value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUpperCase() {
+    this.uppercase = !this.uppercase;
+  }
+
+  onChangeNumbers() {
+    this.numbers = !this.numbers;
+  }
+
+  onChangeSymbols() {
+    this.symbols = !this.symbols;
+  }
+
+  generatePassword() {}
 }
