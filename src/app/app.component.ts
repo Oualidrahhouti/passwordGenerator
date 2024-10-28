e62babf92e0ddbcb2aa6b7401a5ae5773937a846
oualidrahhouti <rahhoutiwalid@gmail.com>
2024-10-28 19:51:28 +0100

    optimising code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -21,24 +22,5 @@ export class AppComponent {
 
   password = '';
 
-  onChangeLength(event: Event) {
-    const parsedValue = parseInt((event.target as HTMLInputElement).value);
-
-    if (!isNaN(parsedValue)) {
-      this.length = parsedValue;
-    }
-  }
-
-  onChangeSettings(settingName: string, settingValue: boolean) {
-    if (
-      settingName !== 'uppercase' &&
-      settingName !== 'numbers' &&
-      settingName !== 'symbols'
-    ) {
-      return;
-    }
-    this[settingName] = settingValue;
-    console.log(this[settingName], settingName);
-  }
   generatePassword() {}
 }
